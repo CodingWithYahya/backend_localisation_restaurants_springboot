a09353bde7f84c6ed781f915bf429d882f74cff1
CodingWithYahya <quazbari.yahya@gmail.com>
2023-06-04 21:56:30 +0100

    first commit

---
+++ src/main/java/com/restau/localisationRes/controllers/ZoneController.java
@@ -0,0 +1,65 @@
+package com.restau.localisationRes.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.restau.localisationRes.entities.Ville;
+import com.restau.localisationRes.entities.Zone;
+import com.restau.localisationRes.services.ZoneService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/zones")
+@CrossOrigin
+public class ZoneController {
+
+	@Autowired(required=true)
+	private ZoneService zoneService;
+
+	@PostMapping("/save")
+	public void save(@RequestBody Zone serie) {
+		zoneService.save(serie);
+	}
+	
+	@PutMapping("/{id}")
+	public void update(@PathVariable int id, @RequestBody Zone zone) {
+	    Zone existingZone = zoneService.findById(id);
+	    if (existingZone != null) {
+	        existingZone.setNom(zone.getNom());
+	        existingZone.setVille(zone.getVille());
+	        zoneService.save(existingZone);
+	    }
+	}		
+
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable(required = true) String id) {
+		Zone s = zoneService.findById(Integer.parseInt(id));
+		zoneService.delete(s);
+	}
+
+	@GetMapping("/all")
+	public List<Zone> findAll() {
+		return zoneService.findAll();
+	}
+
+	//@GetMapping(value = "/count")
+	//public long countZone() {
+	//	return zoneService.count();
+	//}
+
+	@GetMapping("/{id}")
+	public Zone findById(@PathVariable int id) {
+		return zoneService.findById(id);
+	}
+
+	/*
+	@GetMapping("/byFilliere/{id}")
+	public List<Zone> findZonesByVille(@PathVariable int id) {
+		return zoneRepository.findByVilleId(id);
+
+	}
+
+	*/
+	
+}
